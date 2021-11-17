78ccb24bd5c72e4975e9537048ac6a1fdddfbd05
Stepan <gorbatenkostepan@gmail.com>
2021-11-17 18:35:54 +0300

    bbb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'articles#index'
+  root "articles#index"
 
-
-  #get '/articles', to: 'articles#index'
-  #get '/articles/:id', to: 'articles#show'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
+
+
